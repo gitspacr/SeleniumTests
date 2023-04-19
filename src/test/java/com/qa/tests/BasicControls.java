dfd0a226449cda94d75133449ad0c789497f5f35
pranitha <n.pranitha@gmail.com>
2023-04-20 00:04:34 +0530

    latest changes

--- src/test/java/com/qa/tests/BasicControls.java
+++ src/test/java/com/qa/tests/BasicControls.java
@@ -3,6 +3,7 @@ package com.qa.tests;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -30,16 +31,43 @@ public class BasicControls {
 			 driver = new EdgeDriver();
 		}
 		
-		driver.get("https://demo.automationtesting.in/Register.html");
+		driver.get("https://www.google.com");
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		//handling text box
-		driver.findElement(By.xpath("//input[@ng-model='FirstName']")).sendKeys("Selenium");
-		driver.findElement(By.xpath("//input[@ng-model='FirstName']")).clear();
-		driver.findElement(By.xpath("//input[@ng-model='FirstName']")).sendKeys("Automation");
-		String getTxt =driver.findElement(By.xpath("//input[@ng-model='FirstName']")).getAttribute("value");
-		System.out.println(getTxt);
+//		driver.findElement(By.xpath("//input[@ng-model='FirstName']")).sendKeys("Selenium");
+//		driver.findElement(By.xpath("//input[@ng-model='FirstName']")).clear();
+//		driver.findElement(By.xpath("//input[@ng-model='FirstName']")).sendKeys("Automation");
+//		String getTxt =driver.findElement(By.xpath("//input[@ng-model='FirstName']")).getAttribute("value");
+//		System.out.println(getTxt);
+		//print title of the page
+//		String actVal="Register";
+//		String title =driver.getTitle();//expected
+//		System.out.println(title);
+//		if (title.equals("Register")) {
+//			System.out.println("title of the page is verified");
+//		}else {
+//			System.out.println("title of the page is incorrect");
+//		}
+//		String currUrl =driver.getCurrentUrl();
+//		System.out.println(currUrl);
+		//Browser navigation commands - refresh, forward, navigateTo, back
+		driver.findElement(By.name("q")).sendKeys("selenium", Keys.ENTER);
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//h3[text()='Selenium']")).click();
+		driver.navigate().back();
+		Thread.sleep(3000);
+		driver.navigate().forward();
+		Thread.sleep(3000);
+		driver.navigate().refresh();
+		Thread.sleep(3000);
+		driver.navigate().to("https://mvnrepository.com/artifact/org.seleniumhq.selenium/selenium-java");
+		Thread.sleep(3000);
+		driver.navigate().back();
+		Thread.sleep(3000);
+		String currUrl =driver.getCurrentUrl();
+		System.out.println(currUrl);
 		Thread.sleep(3000);
 		driver.quit();
 		
